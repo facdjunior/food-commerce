538eadb4d0ee6f98e4ee33002696669de9c4d9cf
facdjunior <facdjunior@gmail.com>
2023-01-31 11:50:57 -0300

    autenticacao

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { Theme } from './styles/Theme'
 import { GlobalStyle } from './styles/global'
 import { Normalize } from 'styled-normalize'
 
+
 export default function App() {
   return (
     <BrowserRouter>
