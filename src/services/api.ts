da404ca8c679da6d2196c6b41da20b106a7e8df0
facdjunior <facdjunior@gmail.com>
2023-01-31 22:28:05 -0300

    tes

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 
-axios.defaults.withCredentials = false;
+axios.defaults.withCredentials = true;
 const api = axios.create({
   baseURL: process.env.REACT_APP_API_BASE_URL,
 
